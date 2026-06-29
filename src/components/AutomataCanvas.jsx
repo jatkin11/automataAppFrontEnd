9bca91b9c7c4eefa70fd087321f80f3f075a8c6a
Jake Atkins <atkins.jake@icloud.com>
2026-06-29 21:16:40 +0100

    added tool panel, added buttons for tools, and added css for tool panel, and tool buttons, also changed some css for the background of the graph and dots, amended the margin and padding on app.css and index.css to removea buffer in browser

--- src/components/AutomataCanvas.jsx
+++ src/components/AutomataCanvas.jsx
@@ -14,11 +14,23 @@ function AutomataCanvasGraph(){
     return(
         <div className="canvas-graph">
             <ReactFlow>
-            <Background color="#ccc" variant={BackgroundVariant.Dots} />
+            <Background color="#a8a49c" variant={BackgroundVariant.Dots} />
 
             <Panel position="top-left">
                 <div className="app-title">
                     <h1>AutomataApp</h1>
+                </div>
+            </Panel>
+
+            <Panel position="bottom-left">
+                <div className="tool-panel">
+                    <button>Regex → NFA</button>
+                    <button>NFA → DFA</button>
+                    <button>Add node</button>
+                    <button>Minimise DFA</button>
+                    <button>Accepting State</button>
+                    <button>Start State</button>
+                    <button>Delete Tool</button>
                 </div>
             </Panel>
 
