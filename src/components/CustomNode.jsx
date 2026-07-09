fa973e9ee7a9242de993be261f1a92d8ca281d5a
Jake Atkins <atkins.jake@icloud.com>
2026-07-09 20:28:27 +0100

    added custom styling for whether a node is selected, accepted or starting node, amended the CustomNode className to change depending on its data, e.g. starting

--- src/components/CustomNode.jsx
+++ src/components/CustomNode.jsx
@@ -1,9 +1,9 @@
 import {Handle, Position} from "@xyflow/react";
 import "../styles/CustomNode.css";
 
-export default function CustomNode({ data }){
+export default function CustomNode({ data,selected }){
     return (
-        <div className="custom-node">
+        <div className={`custom-node ${selected ? "selected" : ""} ${data.acceptingState ? "accepting":""} ${data.startingState ? "starting":""}`}>
             {data.label}
         <Handle type="source" position={Position.Right}/>
         <Handle type="target" position={Position.Left}/>
