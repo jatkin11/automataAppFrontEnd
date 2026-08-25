0d862c5910ef773bc4b4335e24a972e51b2eac36
Jake Atkins <atkins.jake@icloud.com>
2026-08-25 18:45:51 +0100

    added JSDocs to CustomNode

--- src/components/CustomNode.jsx
+++ src/components/CustomNode.jsx
@@ -1,6 +1,17 @@
 import {Handle, Position} from "@xyflow/react";
 import "../styles/CustomNode.css";
 
+/**
+ * Custom node  
+ * 
+ * Handles on left and right of node
+ * 
+ * Adopted template from ReactFlow documentation (React Flow, 2026a) https://reactflow.dev/api-reference/components/handle
+ * 
+ * @param data node data - acceptingState and startingState determine className to provide different styling for accepting / starting nodes
+ * @param selected node selected boolean
+ * @returns custom node
+ */
 export default function CustomNode({ data,selected }){
     return (
         <div className={`custom-node ${selected ? "selected" : ""} ${data.acceptingState ? "accepting":""} ${data.startingState ? "starting":""}`}>
