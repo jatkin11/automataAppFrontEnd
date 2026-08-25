f2350f35dacd0e26dc721e91e661a95c8591b3ea
Jake Atkins <atkins.jake@icloud.com>
2026-08-25 18:51:10 +0100

    added JSDocs to GlobalNodeIdGenerator

--- src/utilties/GlobalNodeIdGenerator.js
+++ src/utilties/GlobalNodeIdGenerator.js
@@ -1,3 +1,12 @@
+/**
+ * Node ID generator
+ * 
+ * Provides string of next positive available int based on passed nodes array
+ * 
+ * @param nodes array of nodes
+ * @returns string of next non-negative available int 
+ */
+
 export function nextId(nodes){
     let newId = 0;
     while(nodes.some((node) => node.id === String(newId))){
