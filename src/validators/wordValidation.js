d4795e241145893dd3e8a9fe63ec4e11efb87afa
Jake Atkins <atkins.jake@icloud.com>
2026-08-25 21:25:22 +0100

    added JSDocs to wordValidation

--- src/validators/wordValidation.js
+++ src/validators/wordValidation.js
@@ -1,3 +1,15 @@
+/**
+ * Validation for user-inputted Word
+ * 
+ * word cannot contain any white space, only chars listed below
+ * 
+ * word can be blank, as it represents Epsilon
+ * 
+ * @param userInput user-inputted word to test
+ * @returns user-inputted word
+ * @throws {Error} if input is null or word is invalid i.e. not constist only of A-Z,a-z,0-9
+ */
+
 export default function wordValidation(userInput){
  if(userInput === null){
     throw new Error("Word input cannot be null");
