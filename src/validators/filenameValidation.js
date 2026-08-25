545b5574a9065fc54865a77c2de3d73d7f93eeb4
Jake Atkins <atkins.jake@icloud.com>
2026-08-25 21:18:23 +0100

    added JSDocs to filenameValidation

--- src/validators/filenameValidation.js
+++ src/validators/filenameValidation.js
@@ -1,3 +1,18 @@
+
+/**
+ * Validation for User-inputted Filename
+ * 
+ * - returns null if input is null
+ * - trims whitespace
+ * - removes any extenion i.e. anything after a '.'
+ * - checks valid string made of valid chars i.e. A-Z,a-z,0-9, hyphen, underscore
+ * - adds .json to the end
+ * 
+ * 
+ * @param userInput user-inputted filename
+ * @returns normalised filename.json
+ * @throws {Error} if invalid filename
+ */
 export default function filenameValidation(userInput){
  if(userInput == null){
     return null;
@@ -8,8 +23,8 @@ export default function filenameValidation(userInput){
  filename = filename.split(".")[0].trim();
 
  if(!(/^[a-zA-Z0-9_-]+$/.test(filename))){
-    throw new Error("Invalid Filename: Must consist of A-Z, a-z, 0-9, '-', '_' ")
+    throw new Error("Invalid Filename: Must consist of A-Z, a-z, 0-9, '-', '_' ");
  }
- return `${filename}.json`
+ return `${filename}.json`;
 
 }
